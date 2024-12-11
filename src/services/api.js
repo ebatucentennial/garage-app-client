2b117a8e133f0fa7ed8aa9fd5ea6a32e9d1a4e92
Eli <ebatu@my.centennialcollege.ca>
2024-12-10 23:40:57 -0500

    updated api backend url for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: API_URL,
 });
 
 export default instance;
