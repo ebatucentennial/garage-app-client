30da1764da72e5affbf3bbb42e11ba5beb29cedb
Eli <ebatu@my.centennialcollege.ca>
2024-12-12 11:34:08 -0500

    Edited to function locally. We followed the document that we needed to deploy in hosting site thats why do it. As per you request, we edited now to function locally. We just needed to adjust the backed port

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
-
 const instance = axios.create({
-    baseURL: API_URL,
+    baseURL: 'http://localhost:5000',
 });
 
 export default instance;
